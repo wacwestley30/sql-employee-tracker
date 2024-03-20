0664e0205264157a81746f7e40f7872d99198081
wacwestley30 <westleycervantes@gmail.com>
2024-03-20 11:40:39 -0400

    Updated server.js with naming scheme of all planned functions and starting/stopping app. Also updated .gitignore for the createConnection. Need to fill out all current named functions for each action.

---
+++ server.js
@@ -0,0 +1,61 @@
+// imports
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const pass = require('./creds');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: pass,
+    database: 'employee_tracker_db'
+});
+
+const actions = {
+    'View all departments': viewDepartments,
+    'View all roles': viewRoles,
+    'View all employees': viewEmployees,
+    'Add a department': addDepartment,
+    'Add a role': addRole,
+    'Add an employee': addEmployee,
+    'Update an employee role': updateEmployeeRole,
+    'Update employee manager': updateEmployeeManager,
+    'View employees by manager': viewEmployeesByManager,
+    'Delete department': deleteDepartment,
+    'Delete role': deleteRole,
+    'Delete employee': deleteEmployee,
+    'View department budget': viewDepartmentBudget,
+    'Exit': exitApp
+};
+
+function startApp() {
+    inquirer.createPromptModule({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: Object.keys(actions)
+    })
+    .then(answer => {
+        const action = actions[answer.action];
+        if (action) {
+            action();
+        } else {
+            console.log('Invalid action.')
+            startApp();
+        }
+    });
+};
+
+function closeConnection() {
+    db.end(err => {
+        if (err) throw err;
+        console.log('Connection closed.');
+    });
+};
+
+function exitApp() {
+    console.log('Exiting application...');
+    db.closeConnection();
+    process.exit(0);
+};
+
+startApp();
